94fac90edf873c03bd1266cac74215b5eced5666
hyeminem-Jo <konnimey@naver.com>
2023-03-12 15:15:59 +0900

    💄Apply Layout component to edit page

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -2,31 +2,34 @@ import React from 'react';
 import { NextPage } from 'next';
 import Button from "@/components/button";
 import Input from "@/components/input";
+import Layout from "@/components/layout";
 
 const EditProfile: NextPage = () => {
   return (
-    <div className="py-10 px-4 space-y-4">
-      <div className="flex items-center space-x-3">
-        <div className="w-14 h-14 rounded-full bg-slate-500" />
-        <label
-          htmlFor="picture"
-          className="cursor-pointer py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
-        >
-          Change
-          <input id="picture" type="file" className="hidden" accept="image/*" />
-          {/* accept="image/*" -> 이미지 파일만 받을 수 있게 */}
-        </label>
+    <Layout canGoBack title="프로필 수정">
+      <div className="py-10 px-4 space-y-4">
+        <div className="flex items-center space-x-3">
+          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          <label
+            htmlFor="picture"
+            className="cursor-pointer py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
+          >
+            Change
+            <input id="picture" type="file" className="hidden" accept="image/*" />
+            {/* accept="image/*" -> 이미지 파일만 받을 수 있게 */}
+          </label>
+        </div>
+        <Input required label="Email address" name="email" type="email" />
+        <Input
+          required
+          label="Phone number"
+          name="phone"
+          type="number"
+          kind="phone"
+        />
+        <Button text="Upload Profile" />
       </div>
-      <Input required label="Email address" name="email" type="email" />
-      <Input
-        required
-        label="Phone number"
-        name="phone"
-        type="number"
-        kind="phone"
-      />
-      <Button text="Upload Profile" />
-    </div>
+    </Layout>
   );
 };
 
